6a5ac1cce3a10e590b21e78b26965948970f52ff
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2025-01-30 12:32:42 +0200

    Edit eslint config for flexible development

--- eslint.config.js
+++ eslint.config.js
@@ -19,9 +19,12 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
       'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+          'warn',
+          { allowConstantExport: true },
       ],
     },
   },
