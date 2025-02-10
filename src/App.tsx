310e3d6922ed2d95eefbd11b7eca9230ee2eb8a2
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2025-02-10 09:04:41 +0200

    Add the router to code and add button for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Form from "./components/Form/form.component";
+import Appointments from "./components/appointments/appointments.component";
 
 function App() {
   return (
-    <>
-      {/* for test */}
-      <Form />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/doctor" element={<Appointments />} />
+      </Routes>
+    </Router>
   );
 }
 
