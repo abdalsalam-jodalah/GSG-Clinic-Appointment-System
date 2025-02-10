8f3375d5e1e642b90f445376f2314db5e3a3c136
mohammadsalamin <msalameen.123456@gmail.com>
2025-02-11 00:34:07 +0200

    edit package.json and add a main-login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useContext } from "react";
+
 import "./App.css";
-import Login from "./components/Form/Login.component";
-import Header from "./components/Header/Header.component";
-import { AuthContext } from "./providers/authProvider";
+
 
 function App() {
-  const context = useContext(AuthContext);
   return (
     <>
-      {/* <h1 className='text-7xl text-center text-blue-400'>Hello world</h1> */}
-      <Header />
-      {context.user ? (
-        context.user.role === "doctor" ? (
-          "Welcome to Dashboard"
-        ) : context.user.role === "patient" ? (
-          "Welcome to Clinic Appointments"
-        ) : null
-      ) : (
-        <Login />
-      )}
+
     </>
   );
 }
